61cd78c1eddd945a4c54a52ad0762333f338452f
Purshya027 <pchinchane5@gmail.com>
2025-11-14 09:39:34 +0530

    Add files via upload
    Day 1 Class Learning

---
+++ day1/LearnPrimitiveDatatype.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class LearnPrimitiveDatatype {
+
+	public static void main(String[] args) {
+		byte num=1;
+		System.out.println(num);
+		
+		short age=35;
+		System.out.println(age);
+		
+		int year=2025;
+		System.out.println(year);
+		
+		long contact=7676436346L;
+		System.out.println(contact);
+		
+		float currency1=(8.123456789f);
+		System.out.println(currency1);
+		
+		double currency2=(82.123456789);
+		System.out.println(currency2);
+		
+		char a='t';
+		System.out.println(a);
+		
+		boolean happy=false;
+		System.out.println(happy);
+		
+		String name="Purushottam";
+		System.out.println("My name is "+name+"."+" "+"My age is "+age+"."+" "+"My contact is "+contact);
+	}
+
+}
